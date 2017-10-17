219d73117ef79ff05a3273fc33ddb49fcdc2902a
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-17 00:24:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+katz_deli = []
+
+def line (deli_line)
+  if deli_line.length == 0
+    puts "The line is currently empty."
+  else
+    deli_line.each_with_index.map {|x, i| "The line is currently #{i + 1}. #{x}"}
+  end
+end
+
+def take_a_number (current_line, new_person)
+  if current_line.length == 0
+    return current_line.unshift(new_person)
+    "Welcome #{new_person}. You are number 1 in line."
+  else
+    current_line.push(new_person)
+    return "Welcome #{new_person}. You are number #{current_line.length} in line."
+  end
+end
