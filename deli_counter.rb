c652cdb3f85fe3d8593e5ddc9aa8f3afabcf5024
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-17 00:44:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,11 +10,6 @@ def line (deli_line)
 end
 
 def take_a_number (current_line, new_person)
-  if current_line.length == 0
-    current_line.push(new_person)
-    puts "Welcome, #{new_person}. You are number 1 in line."
-  else
     current_line.push(new_person)
     puts "Welcome, #{new_person}. You are number #{current_line.length} in line."
-  end
 end
