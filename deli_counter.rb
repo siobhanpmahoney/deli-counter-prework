3136580f2a0d239d00a125ee79910796f163e6d7
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-17 00:26:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line (deli_line)
   if deli_line.length == 0
     puts "The line is currently empty."
   else
-    deli_line.each_with_index.map {|x, i| "The line is currently: #{i + 1}. #{x}"}
+    deli_line.each_with_index.map {|x, i| puts "The line is currently: #{i + 1}. #{x}"}
   end
 end
 
